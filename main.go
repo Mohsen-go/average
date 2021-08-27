dde4d87e61c37a1d4b6fb7bbbe4ea6bb8c2e3f20
Mohsen <89256990+Mohsen-go@users.noreply.github.com>
2021-08-27 19:00:26 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main()  {
+
+	numbers :=[3] float64{71.8,56.2,89.5}
+
+	var sum float64 = 0
+
+	for _ ,numbers := range numbers{
+		sum += numbers
+	}
+	fmt.Println(sum)
+	
+}
